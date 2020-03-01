7070a96f04f5f1a7ca7f232918aadd170760c2b3
stockera <arnosto@hotmail.fr>
2020-03-01 21:08:37 +0100

    initialisation ACarre

--- source/ACarre.java
+++ source/ACarre.java
@@ -1,2 +1,11 @@
-public class ACarre {
+public class ACarre implements Formula {
+    @Override
+    public String asString() {
+        return null;
+    }
+
+    @Override
+    public Tree asTree() {
+        return null;
+    }
 }
