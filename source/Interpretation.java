46d7f8f4045ea1fc61247411d3b93a79b1237eef
SophiePaillocher <sophie.paillocher@hotmail.fr>
2020-03-04 14:08:29 +0100

    rename

--- source/Interpretation.java
+++ source/Interpretation.java
@@ -3,57 +3,57 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class Interpretation {
-    private ArrayList<String> s ;
-    private HashMap<String, ArrayList<String>> l;
-    private HashMap<String, ArrayList<String>> r;
+    private ArrayList<String> vertices;
+    private HashMap<String, ArrayList<String>> labels;
+    private HashMap<String, ArrayList<String>> relations;
 
     public void Interpretation () {
-        s = new ArrayList<>();
-        l = new HashMap<>();
-        r = new HashMap<>();
+        vertices = new ArrayList<>();
+        labels = new HashMap<>();
+        relations = new HashMap<>();
     }
-    public void Interpretation (ArrayList<String> vertexes){
-        this.s = vertexes;
-        this.l = new HashMap<>();
-        this.r = new HashMap<>();
-    }
-
-    public void Interpretation (ArrayList<String> vertexes, HashMap<String, ArrayList<String>> labels, HashMap<String, ArrayList<String>> relations){
-        this.s = vertexes;
-        this.l = labels;
-        this.r = relations;
+    public void Interpretation (ArrayList<String> verteces){
+        this.vertices = verteces;
+        this.labels = new HashMap<>();
+        this.relations = new HashMap<>();
     }
 
-    public ArrayList<String> getS() {
-        return s;
+    public void Interpretation (ArrayList<String> verteces, HashMap<String, ArrayList<String>> labels, HashMap<String, ArrayList<String>> relations){
+        this.vertices = verteces;
+        this.labels = labels;
+        this.relations = relations;
     }
 
-    public HashMap<String, ArrayList<String>> getL() {
-        return l;
+    public ArrayList<String> getVertices() {
+        return vertices;
     }
 
-    public HashMap<String, ArrayList<String>> getR() {
-        return r;
+    public HashMap<String, ArrayList<String>> getLabels() {
+        return labels;
     }
 
-    public void setL(HashMap<String, ArrayList<String>> l) {
-        this.l = l;
+    public HashMap<String, ArrayList<String>> getRelations() {
+        return relations;
     }
 
-    public void setR(HashMap<String, ArrayList<String>> r) {
-        this.r = r;
+    public void setLabels(HashMap<String, ArrayList<String>> labels) {
+        this.labels = labels;
     }
 
-    public void setS(ArrayList<String> s) {
-        this.s = s;
+    public void setRelations(HashMap<String, ArrayList<String>> relations) {
+        this.relations = relations;
+    }
+
+    public void setVertices(ArrayList<String> vertices) {
+        this.vertices = vertices;
     }
 
     public ArrayList<String> getSuccesseurs(String sommet){
-        return r.get(sommet);
+        return relations.get(sommet);
     }
 
     public ArrayList<String> getMarquings(String sommet){
-        return l.get(sommet);
+        return labels.get(sommet);
     }
 
     @Override
@@ -61,22 +61,22 @@ public class Interpretation {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Interpretation that = (Interpretation) o;
-        return s.equals(that.s) &&
-                l.equals(that.l) &&
-                r.equals(that.r);
+        return vertices.equals(that.vertices) &&
+                labels.equals(that.labels) &&
+                relations.equals(that.relations);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(s, l, r);
+        return Objects.hash(vertices, labels, relations);
     }
 
     @Override
     public String toString() {
         return "Interpretation{" +
-                "s=" + s +
-                ", l=" + l +
-                ", r=" + r +
+                "vertices=" + vertices +
+                ", labels=" + labels +
+                ", relations=" + relations +
                 '}';
     }
 
